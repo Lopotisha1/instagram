66d9453cb16184ee275d24dad4076a66afc5f8c6
Lopotisha1 <vano1300vano1300@gmail.com>
2024-11-18 02:39:53 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,19 +1,20 @@
-<?php
-
-$username = isset($_POST['username']) ? $_POST['username'] : 'Unknown';
-$password = isset($_POST['password']) ? $_POST['password'] : 'Unknown';
-
-
-$fp = fopen('log.txt', 'a+');
-if ($fp) {
- 
-    fwrite($fp, "Username: " . htmlspecialchars($username) . "\n");
-    fwrite($fp, "Password: " . htmlspecialchars($password) . "\n");
-    fwrite($fp, "----------------\n");
-
-  
-    fclose($fp);
-} else {
-    echo "Ошибка при открытии файла.";
-}
-?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Форма входа</title>
+</head>
+<body>
+    <h1>Вход в систему</h1>
+    <form action="your_php_script.php" method="POST">
+        <label for="username">Имя пользователя:</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>
+        <label for="password">Пароль:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <input type="submit" value="Отправить">
+    </form>
+</body>
+</html>
